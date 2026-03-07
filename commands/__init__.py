08f39be10be4928a68781a2b99ca3ad7a134ba73
Riley Schillinger <70186143+Rileyelir@users.noreply.github.com>
2026-03-07 14:57:28 -0500

    Work on server creator & rename c_setup to c_creator
    The server creator now has points for the user to input data like the server name, version, and upper DRAM limit. It creates a folder for the server, downloads the jar in there, and the start script is also auto-generated.

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,5 +1,5 @@
 __all__ = [
     "c_small",
-    "c_setup",
+    "c_creator",
     "c_manager"
 ]
--- commands/c_creator.py
+++ commands/c_creator.py
@@ -1,22 +1,36 @@
-# c_setup.py
+# c_creator.py
 # Handles the creation of new servers by downloading the chosen server file, auto-generating a start script, and automatically adding it to mt_data.json.
 
 # Imports
 import requests
-import json
+from os import mkdir
 
 # Functions
-def download_jar(version: str):
+def create_server():
+    print("\n-- Server Creator --")
+    name = input("Server Name: ")
+    version = input("Server Version (i.e. 1.12, 1.21.11): ")
+
+    mkdir(name)
+    download_vanilla_jar(version, name)
+
+    maxRam = input("\nServer Ram Limit (gb): ")
+    with open(f"{name}/start.bat", "w") as file:
+        file.write(f"java -Xmx{maxRam}G -Xms{maxRam}G -jar server.jar")
+
+def download_vanilla_jar(version, path):
     vanillaManifest = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json").json()
     for v in vanillaManifest["versions"]:
         if v["id"] == version:
-            print("[SUCCESS] Version found, downloading...")
+            print("\n[SUCCESS] Version found, downloading...")
             versionInfo = requests.get(v["url"]).json()
             serverJar = requests.get(versionInfo["downloads"]["server"]["url"], stream=True)
-            with open("MinecraftServer_" + version + ".jar", "wb") as file:
+            with open(f"{path}/server.jar", "wb") as file:
                 for chunk in serverJar.iter_content(chunk_size=8192):
                     file.write(chunk)
             print("[SUCCESS] Server file downloaded successfully.")
+            return
+    print("[ERROR] Version not found or other error occurred.")
 
 # Testing Code (ran when running c_setup.py separately)
-download_jar("1.17.1")
+create_server()
